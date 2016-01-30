11939dece9733caec623d7d000f35e802cd413a6
Jmeas <jellyes2@gmail.com>
2016-01-29 23:16:53 -0500

    Add hbs to server
    Resolves #8

--- server/index.js
+++ server/index.js
@@ -1,12 +1,28 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
+
+const BASE_DIR = __dirname;
+const VIEWS_DIR = path.join(BASE_DIR, 'views');
 
 const app = express();
+
+// Configure the templating engine
+const hbsOptions = {
+  extname: '.hbs',
+  layoutsDir: VIEWS_DIR + '/layouts',
+  partialsDir: VIEWS_DIR + '/partials',
+  defaultLayout: 'main'
+};
+app.set('view engine', '.hbs');
+app.set('views', VIEWS_DIR);
+app.engine('.hbs', exphbs(hbsOptions));
 
 const port = process.env.PORT || 4321;
 app.set('port', port);
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  return res.render('index');
 });
 
 app.listen(port, function() {
