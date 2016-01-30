e0334c3a6d6b81d689469fac6f946695c19831ad
Jmeas <jellyes2@gmail.com>
2016-01-30 15:58:50 -0500

    Add clustering for future scaling
    Resolves #10

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const compress = require('compression');
+const throng = require('throng');
 
 // Heroku sets NODE_ENV to production by default. So if we're not
 // on Heroku, we assume that we're developing locally.
@@ -11,33 +12,42 @@ const PROJECT_ROOT = path.normalize(BASE_DIR + '/..');
 const ASSETS_PATH = path.join(PROJECT_ROOT, 'client-dist');
 const VIEWS_DIR = path.join(BASE_DIR, 'views');
 
-const app = express();
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
-app.set('env', NODE_ENV);
+function start() {
+  const app = express();
 
-app.use(compress());
-app.use(express.static(ASSETS_PATH));
+  app.set('env', NODE_ENV);
 
-// Configure the templating engine
-const hbsOptions = {
-  extname: '.hbs',
-  layoutsDir: VIEWS_DIR + '/layouts',
-  partialsDir: VIEWS_DIR + '/partials',
-  defaultLayout: 'main'
-};
-app.set('view engine', '.hbs');
-app.set('views', VIEWS_DIR);
-app.engine('.hbs', exphbs(hbsOptions));
+  app.use(compress());
+  app.use(express.static(ASSETS_PATH));
 
-const port = process.env.PORT || 5000;
-app.set('port', port);
+  // Configure the templating engine
+  const hbsOptions = {
+    extname: '.hbs',
+    layoutsDir: VIEWS_DIR + '/layouts',
+    partialsDir: VIEWS_DIR + '/partials',
+    defaultLayout: 'main'
+  };
+  app.set('view engine', '.hbs');
+  app.set('views', VIEWS_DIR);
+  app.engine('.hbs', exphbs(hbsOptions));
 
-// Every route is served by our JS app
-app.get('*', function(req, res) {
-  res.locals.devMode = res.app.get('env') === 'development';
-  return res.render('index');
-});
+  const port = process.env.PORT || 5000;
+  app.set('port', port);
 
-app.listen(port, function() {
-  console.log('Node app is running at localhost:' + port);
+  // Every route is served by our JS app
+  app.get('*', function(req, res) {
+    res.locals.devMode = res.app.get('env') === 'development';
+    return res.render('index');
+  });
+
+  app.listen(port, function() {
+    console.log('Node app is running at localhost:' + port);
+  });
+}
+
+throng(start, {
+  workers: WORKERS,
+  lifetime: Infinity
 });
