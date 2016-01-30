e03b8057781ec7e631fd002c773f896af89d96ca
Jmeas <jellyes2@gmail.com>
2016-01-30 11:37:11 -0500

    Adds basic React router configuration
    Resolves #37

---
+++ client-src/components/layout/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Layout = ({children}) => (
+  <div>
+    <h1>Layout Pls</h1>
+    {children}
+  </div>
+);
+
+export default Layout;
--- client-src/finance-app.js
+++ client-src/finance-app.js
@@ -1,7 +1,19 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+
+import Layout from './components/layout';
+import HomePage from './components/home-page';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path='/' component={Layout}>
+      <IndexRoute component={HomePage} />
+    </Route>
+  </Router>
+);
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  routes,
   document.querySelector('main')
 );
