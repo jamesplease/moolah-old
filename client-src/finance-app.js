320fe0487afe51ebe48c4639e0fba9d604f02b62
Jmeas <jellyes2@gmail.com>
2016-01-30 12:04:40 -0500

    Update to use pushState
    Resolves #43

--- client-src/finance-app.js
+++ client-src/finance-app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+import {Router, Route, IndexRoute} from 'react-router';
 
 import Layout from './components/layout';
 import HomePage from './components/home-page';
 import NotFound from './components/not-found';
 
 const routes = (
-  <Router history={browserHistory}>
+  <Router history={createBrowserHistory()}>
     <Route path='/' component={Layout}>
       <IndexRoute component={HomePage} />
       <Route path="*" component={NotFound}/>
