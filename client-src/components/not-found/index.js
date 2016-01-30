8fd377951106800fbb55dca60e87c78b457bf4f6
Jmeas <jellyes2@gmail.com>
2016-01-30 12:00:59 -0500

    Add basic notFound route

---
+++ client-src/components/not-found/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    Sorry, that page could not be found.
+  </div>
+);
+
+export default HomePage;
--- client-src/finance-app.js
+++ client-src/finance-app.js
@@ -4,11 +4,13 @@ import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 
 import Layout from './components/layout';
 import HomePage from './components/home-page';
+import NotFound from './components/not-found';
 
 const routes = (
   <Router history={browserHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={HomePage} />
+      <Route path="*" component={NotFound}/>
     </Route>
   </Router>
 );
