71dc5a5c8ec5df0048cad685b35eaf7aff151bdb
Jmeas <jellyes2@gmail.com>
2016-01-31 18:00:42 -0500

    Add placeholder terms and about pages

--- client-src/index.js
+++ client-src/index.js
@@ -7,11 +7,15 @@ import {Router, Route, IndexRoute} from 'react-router';
 import Layout from './pods/common/components/layout';
 import NotFound from './pods/common/components/not-found';
 import HomePage from './pods/home/components/home-page';
+import About from './pods/meta/components/about';
+import Terms from './pods/meta/components/terms';
 
 const routes = (
   <Router history={createBrowserHistory()}>
     <Route path='/' component={Layout}>
-      <IndexRoute component={HomePage} />
+      <IndexRoute component={HomePage}/>
+      <Route path='/about' component={About}/>
+      <Route path='/terms' component={Terms}/>
       <Route path='*' component={NotFound}/>
     </Route>
   </Router>
--- client-src/pods/common/components/footer/index.js
+++ client-src/pods/common/components/footer/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import FooterNav from '../footer-nav';
+
 const Footer = () => (
   <footer>
     <div className="container">
       Created by Jmeas. Source code available on <a href="https://github.com/jmeas/finance-app">GitHub</a>.
+      <FooterNav/>
     </div>
   </footer>
 );
