08b141e32e6178fda5d47328b99cf78ceab253cb
Jmeas <jellyes2@gmail.com>
2016-01-31 18:13:15 -0500

    Add more data to the transaction list. Add Material design icons

--- client-src/pods/home/components/transaction/index.js
+++ client-src/pods/home/components/transaction/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Transaction = ({id, name}) => (
-  <li key={id}>{name}</li>
+  <li className="transaction-row" key={id}>
+    <span className="transaction-list-cell">
+      03/01
+    </span>
+    <span className="transaction-list-cell">
+      {name}
+    </span>
+    <span className="transaction-list-cell">
+      $0.00
+    </span>
+    <span className="transaction-list-cell">
+      <i className="zmdi zmdi-delete"></i>
+    </span>
+  </li>
 );
 
 Transaction.propTypes = {
