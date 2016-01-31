b2ae92ae334e336ecdf4772c49209b5faef20260
Jmeas <jellyes2@gmail.com>
2016-01-31 16:46:44 -0500

    Fetch data in home component
    Displays a loading message when the app first loads. Then, when the
    data returns, it shows the number of items retrieved.

--- client-src/pods/home/components/home-page/index.js
+++ client-src/pods/home/components/home-page/index.js
@@ -1,9 +1,48 @@
 import React from 'react';
 
-const HomePage = () => (
-  <div>
-    Welcome home!
-  </div>
-);
+export default class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fetched: false,
+      transactions: []
+    };
+  }
+
+  componentDidMount() {
+    window.setTimeout(() => {
+      this.fetchData();
+    }, 1000);
+  }
+
+  // We set unmounted so that we can ignore data requests
+  componentWillUnmount () {
+    this.unmounted = true;
+  }
+
+  fetchData() {
+    fetch('/tests')
+      .then(
+        res => res.json()
+      )
+      .then(json => {
+        // Only set the state if the component is still mounted
+        if (this.unmounted) { return; }
+        this.setState({
+          fetched: true,
+          transactions: json.data
+        });
+      })
+      .catch(() => console.error('There was an error while retrieving data.'));
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.fetched ? 'Loading...' : `Fetched ${this.state.transactions.length} items!`}
+      </div>
+    );
+  }
+}
 
 export default HomePage;
