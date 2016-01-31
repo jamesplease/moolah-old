4618a287dd3a7416f7b3a5098a0c09999588adae
Jmeas <jellyes2@gmail.com>
2016-01-31 11:13:27 -0500

    Podify client

--- client-src/index.js
+++ client-src/index.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import {Router, Route, IndexRoute} from 'react-router';
 
-import Layout from './components/layout';
-import HomePage from './components/home-page';
-import NotFound from './components/not-found';
+import Layout from './pods/common/components/layout';
+import NotFound from './pods/common/components/not-found';
+import HomePage from './pods/home/components/home-page';
 
 const routes = (
   <Router history={createBrowserHistory()}>
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -26,7 +26,17 @@ const exportFileName = path.basename(mainFile, path.extname(mainFile));
 var working = false;
 
 function stylus() {
-  return gulp.src('client-src/stylus/index.styl')
+  return gulp.src([
+      // Load our entrypoint first. This gives us an opportunity to load 3rd-party libraries,
+      // or whatever else needs to be loaded first
+      './client-src/index.styl',
+      // Next, we load the common stylus files. These might be depended upon by other files
+      './client-src/pods/common/stylus/**/*.styl',
+      // // Lastly, we pull in the stylus out of every pod. Variables are always loaded first, so
+      // // that each pod can define its own variables
+      './client-src/pods/**/variables.styl',
+      './client-src/pods/**/*.styl'
+    ])
     .pipe($.sourcemaps.init())
     .pipe($.stylus({
       compress: productionMode
