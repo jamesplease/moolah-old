8830ad7bfd38853b1b245eef896cc7eb6ffa3975
Jmeas <jellyes2@gmail.com>
2016-01-31 18:53:21 -0500

    Add client-side delete functionality to a transaction

--- client-src/pods/home/components/transaction/index.js
+++ client-src/pods/home/components/transaction/index.js
@@ -1,25 +1,40 @@
 import React from 'react';
 
-const Transaction = ({id, name}) => (
-  <li className="transaction-row" key={id}>
-    <span className="transaction-list-cell">
-      03/01
-    </span>
-    <span className="transaction-list-cell">
-      {name}
-    </span>
-    <span className="transaction-list-cell">
-      $0.00
-    </span>
-    <span className="transaction-list-cell">
-      <i className="zmdi zmdi-delete"></i>
-    </span>
-  </li>
-);
+class Transaction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: props.id,
+      name: props.name
+    };
+
+    this.deleteFn = props.deleteFn;
+  }
+
+  render() {
+    return (
+      <li className="transaction-row" key={this.state.id}>
+        <div className="transaction-list-cell transaction-date-cell">
+          03/01
+        </div>
+        <div className="transaction-list-cell transaction-name-cell">
+          {this.state.name}
+        </div>
+        <div className="transaction-list-cell transaction-value-cell">
+          $0.00
+        </div>
+        <div className="transaction-list-cell transaction-delete-cell">
+          <i className="zmdi zmdi-delete delete-transaction" onClick={this.deleteFn.bind(this, this.state.id)}></i>
+        </div>
+      </li>
+    );
+  }
+}
 
 Transaction.propTypes = {
   id: React.PropTypes.number,
   name: React.PropTypes.string,
+  deleteFn: React.PropTypes.func
 };
 
 export default Transaction;
--- client-src/pods/home/components/transactions-list/index.js
+++ client-src/pods/home/components/transactions-list/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 import Transaction from '../transaction';
 
-const TransactionsList = ({transactions}) => (
-  <div>
-    <ul>
-      {transactions.map(({id, name}) => (
-        <Transaction key={id} id={id} name={name}/>
-      ))}
-    </ul>
-  </div>
-);
+class TransactionsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = props;
+  }
+
+  deleteTransaction(id) {
+    // Optimistically remove the item from the state
+    this.setState({
+      transactions: this.state.transactions.filter(t => t.id !== id)
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.transactions.map(({id, name}) => (
+            <Transaction key={id} id={id} name={name} deleteFn={this.deleteTransaction.bind(this)}/>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 TransactionsList.propTypes = {
   transactions: React.PropTypes.array,
