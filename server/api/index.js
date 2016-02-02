b17efa8932fc6f7dc3f37f18160a27f79c088fec
Jmeas <jellyes2@gmail.com>
2016-02-02 10:15:48 -0500

    Remove test API endpoints
    Resolves #79

--- server/api/index.js
+++ server/api/index.js
@@ -2,14 +2,12 @@ const express = require('express');
 
 const migrate = require('./util/migrate');
 const dbConnect = require('./db-connect');
-const tests = require('./pods/tests');
 const transactions = require('./pods/transactions');
 
 migrate.up();
 
 const router = express.Router();
 
-router.use('/tests', tests);
 router.use('/transactions', transactions);
 
 module.exports = router;
