5d46f87d08e3f9ef5d957379cb638cfad2b36913
Jmeas <jellyes2@gmail.com>
2016-02-03 22:21:52 -0500

    Restructure integration test to load fixture data in beforeEach

--- test/integration/api/transactions/index.js
+++ test/integration/api/transactions/index.js
@@ -13,49 +13,26 @@ function getInsertQuery(values) {
   };
 }
 
-describe('A mock test', () => {
-  it('should pass', done => {
-    const query = getInsertQuery({
-      value: '10.20'
-    });
+describe('Transactions', () => {
+  // This loads our fixture data
+  beforeEach(() => {
+    const queries = [
+      getInsertQuery({ value: '10.20'}),
+      getInsertQuery({ value: '1000.20', description: 'test'})
+    ];
 
-    var db = pgp(dbConfig());
-
-    db.none(query)
-      .then(() => {
-        return db.any(`SELECT * FROM ${TABLE_NAME}`)
-          .then(result => {
-            expect(result).to.have.length(1);
-            done();
-          });
-      })
-      .catch(() => {
-        throw new Error('fail');
-        done();
-      });
+    const db = pgp(dbConfig());
+    return Promise.all(queries.map(q => db.none(q)));
   });
-});
 
-describe('Another mock test', () => {
-  it('should pass', done => {
-    const query = getInsertQuery({
-      value: '1000.00',
-      description: 'test'
+  describe('A mock test', () => {
+    it('should pass', () => {
+      var db = pgp(dbConfig());
+
+      return db.any(`SELECT * FROM ${TABLE_NAME}`)
+        .then(result => {
+          expect(result).to.have.length(2);
+        });
     });
-
-    var db = pgp(dbConfig());
-
-    db.none(query)
-      .then(() => {
-        return db.any(`SELECT * FROM ${TABLE_NAME}`)
-          .then(result => {
-            expect(result).to.have.length(1);
-            done();
-          });
-      })
-      .catch(() => {
-        throw new Error('fail');
-        done();
-      });
   });
 });
