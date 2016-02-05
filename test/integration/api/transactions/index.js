5089d154dfa437e0f3c235511101f58d373d9c96
Jmeas <jellyes2@gmail.com>
2016-02-05 17:25:45 -0500

    Add POST /transactions integration tests

--- test/integration/api/transactions/index.js
+++ test/integration/api/transactions/index.js
@@ -51,7 +51,6 @@ function dataEquals(obj, res) {
 
 function errorsEquals(obj, res) {
   if (!_.isEqual(res.body.errors, obj)) {
-    console.log('wat', res.body.errors, obj);
     return new Error('The errors did not match.');
   }
 }
@@ -426,6 +425,103 @@ describe('Transactions', () => {
           .set('Accept', 'application/json')
           .expect(204)
           .expect(isEmpty)
+          .end(function(err, res) {
+            if (err) { return done(err); }
+            done();
+          });
+      });
+    });
+  });
+
+  describe('POST /transactions', () => {
+    describe("when the request is empty, thus failing validation", () => {
+      it('should return 400', done => {
+          request(app())
+            .post('/transactions')
+            .set('Accept', 'application/json')
+            .expect(400)
+            .end(function(err, res) {
+              if (err) { return done(err); }
+              done();
+            });
+        });
+
+        it('should return the correct error', done => {
+          const errors = [{
+            statusCode: '400',
+            title: 'Bad Request',
+            detail: '"value" is required'
+          }];
+
+          request(app())
+            .post('/transactions')
+            .set('Accept', 'application/json')
+            .expect(_.partial(errorsEquals, errors))
+            .end(function(err, res) {
+              if (err) { return done(err); }
+              done();
+            });
+        });
+    });
+
+    describe("when the date fails validation", () => {
+      it('should return 400', done => {
+          request(app())
+            .post('/transactions')
+            .set('Accept', 'application/json')
+            .send({value: '20.04', date: 'not a date'})
+            .expect(400)
+            .end(function(err, res) {
+              if (err) { return done(err); }
+              done();
+            });
+        });
+
+        it('should return the correct error', done => {
+          const errors = [{
+            statusCode: '400',
+            title: 'Bad Request',
+            detail: '"date" must be date format'
+          }];
+
+          request(app())
+            .post('/transactions')
+            .set('Accept', 'application/json')
+            .send({value: '20.04', date: 'not a date'})
+            .expect(_.partial(errorsEquals, errors))
+            .end(function(err, res) {
+              if (err) { return done(err); }
+              done();
+            });
+        });
+    });
+
+    describe("when the request is valid", () => {
+      it('should return 201', done => {
+          request(app())
+            .post('/transactions')
+            .set('Accept', 'application/json')
+            .send({value: '20.04', date: '2015-10-12'})
+            .expect(201)
+            .end(function(err, res) {
+              if (err) { return done(err); }
+              done();
+            });
+      });
+
+      it('should return the created resource', done => {
+        const data = {
+          id: 1,
+          value: '20.04',
+          date: '2015-10-12',
+          description: null
+        };
+
+        request(app())
+          .post('/transactions')
+          .set('Accept', 'application/json')
+          .send({value: '20.04', date: '2015-10-12'})
+          .expect(_.partial(dataEquals, data))
           .end(function(err, res) {
             if (err) { return done(err); }
             done();
