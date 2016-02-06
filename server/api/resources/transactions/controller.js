1731d79384b51886f1fc203648a18027be70f56a
Jmeas <jellyes2@gmail.com>
2016-02-05 23:57:27 -0500

    Add create method to controller; attach options to instance

--- server/api/resources/transactions/controller.js
+++ server/api/resources/transactions/controller.js
@@ -1,11 +1,19 @@
+const _ = require('lodash');
+
+// The options that can be passed into a Controller
+const validOptions = ['table', 'store'];
+
 // A controller represents an interface to the data
 // stored in our database
 function Controller(options) {
-  this.options = options;
+  Object.assign(this, _.pick(options, validOptions));
 }
 
 Object.assign(Controller.prototype, {
-  create() {},
+  create(data) {
+    const query = `INSERT INTO ${this.table} (description, value, date) VALUES ($1, $2, $3) RETURNING *`;
+    return this.store.one(query, [data.description, data.value, data.date]);
+  },
 
   read(id) {
     // `singular` is whether or not we're looking for 1
@@ -13,7 +21,7 @@ Object.assign(Controller.prototype, {
     const singular = Boolean(id);
 
     // Our base query
-    var query = `SELECT * FROM ${this.options.table}`;
+    var query = `SELECT * FROM ${this.table}`;
 
     // If we're looking for one, we modify the query string
     if (singular) {
@@ -21,7 +29,7 @@ Object.assign(Controller.prototype, {
     }
 
     const method = singular ? 'oneOrNone' : 'any';
-    return this.options.store[method](query, id);
+    return this.store[method](query, id);
   },
 
   update() {},
@@ -31,8 +39,8 @@ Object.assign(Controller.prototype, {
       return Promise.reject(new TypeError('id was undefined'));
     }
 
-    const query = `DELETE FROM ${this.options.table} WHERE id = $1 RETURNING *`;
-    return this.options.store.one(query, id);
+    const query = `DELETE FROM ${this.table} WHERE id = $1 RETURNING *`;
+    return this.store.one(query, id);
   }
 });
 
