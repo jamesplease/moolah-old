252c3eda6c636440a025d5fb3611912d3a14ce79
Jmeas <jellyes2@gmail.com>
2016-04-09 22:07:13 -0700

    Set up CORS on the API

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 
 const migrate = require('./util/migrate');
 const transactions = require('./resources/transactions');
@@ -9,6 +10,7 @@ migrate.up();
 
 const router = express.Router();
 
+router.all('*', cors());
 router.use('/transactions', transactions);
 
 module.exports = router;
