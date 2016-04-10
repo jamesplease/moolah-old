fc6286c28b51d140a37c9c01924c18dab1f2a3a5
Jmeas <jellyes2@gmail.com>
2016-04-09 22:24:56 -0700

    Update pg-promise and error logic
    Resolves #144, #138

--- server/api/util/catch-rejected-query.js
+++ server/api/util/catch-rejected-query.js
@@ -4,11 +4,20 @@ const generateErrors = require('../errors/generate-errors');
 
 // Our generic `.catch()` for failed promises from pg-query
 function catchRejectedQuery(res, e) {
-  if (e.message === 'No data returned from the query.') {
+  // The only error that gets special treatment is `0`, which is when we expect
+  // data but get nothing back. This means that the resource wasn't found.
+  // These codes are defined here:
+  // https://github.com/vitaly-t/pg-promise/blob/402283eef41e8788f56f59511d3cb1053decbfee/lib/errors.js#L5-L28
+  if (e.code === 0) {
     res.status(404).send({
       errors: [generateErrors.notFoundError()]
     });
-  } else {
+  }
+  // The two other error codes are for when we expect nothing, and get
+  // something, or when we wanted a single row and instead were returned
+  // multiple. These are indicative of app-level errors, so we return a generic
+  // 500 response.
+  else {
     res.status(500).send({
       errors: [generateErrors.genericError()]
     });
