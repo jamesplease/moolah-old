e91fe008fb9088e50ff128eefd103e034aaac217
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-04-10 07:10:03 +0100

    Update catch-rejected-query.js
    this is how it should be ;)

--- server/api/util/catch-rejected-query.js
+++ server/api/util/catch-rejected-query.js
@@ -2,13 +2,18 @@
 
 const generateErrors = require('../errors/generate-errors');
 
+const errors = require('pg-promise').errors;
+
+const QueryResultError = errors.QueryResultError;
+const qrec = errors.queryResultErrorCode;
+
 // Our generic `.catch()` for failed promises from pg-query
 function catchRejectedQuery(res, e) {
   // The only error that gets special treatment is `0`, which is when we expect
   // data but get nothing back. This means that the resource wasn't found.
   // These codes are defined here:
   // https://github.com/vitaly-t/pg-promise/blob/402283eef41e8788f56f59511d3cb1053decbfee/lib/errors.js#L5-L28
-  if (e.code === 0) {
+  if (e instanceof QueryResultError && e.code === qrec.noData) {
     res.status(404).send({
       errors: [generateErrors.notFoundError()]
     });
