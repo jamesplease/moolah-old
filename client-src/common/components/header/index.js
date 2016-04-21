80135e13caf6d37bd84ff7050426451aefa9787d
Jmeas <jellyes2@gmail.com>
2016-04-20 19:34:47 -0700

    Begin work toward basic styling of app

--- client-src/common/components/header/index.js
+++ client-src/common/components/header/index.js
@@ -1,14 +1,22 @@
 import yo from 'yo-yo';
+import store from '../../../redux/store';
+import {createTransaction} from '../../../redux/transactions/action-creators';
+
+function onClickNew() {
+  store.dispatch(createTransaction({
+    value: Math.random(),
+    description: 'Hot off the press',
+    date: '2015-10-02'
+  }));
+}
 
 export default function() {
   return yo`
     <header className="app-header">
       <div className="container">
-        <h1 className="app-logo">
-          <a href="/" className="app-logo-link">
-            Finance App
-          </a>
-        </h1>
+        <button className="create-new-transaction" onclick=${onClickNew}>
+          + New Transaction
+        </button>
       </div>
     </header>
   `;
