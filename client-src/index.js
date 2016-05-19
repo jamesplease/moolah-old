daea6457c4cad60386e3edbee883e50c79eb5628
Jmeas <jellyes2@gmail.com>
2016-05-18 19:40:59 -0700

    Add React to dependencies. Set up basic React Router system.

--- client-src/index.js
+++ client-src/index.js
@@ -1,84 +1,17 @@
 import 'babel-polyfill';
-import yo from 'yo-yo';
-import sheetRouter from 'sheet-router';
-import history from 'sheet-router/history';
-import href from 'sheet-router/href';
 
-import layout from './common/components/layout';
-import dashboardSubheader from './dashboard/components/subheader';
-import dashboard from './dashboard/components/dashboard';
-import categoriesSubheader from './categories/components/subheader';
-import categories from './categories/components/content';
-import profileSubheader from './profile/components/subheader';
-import profile from './profile/components/profile';
-import analyticsSubheader from './analytics/components/subheader';
-import analytics from './analytics/components/analytics';
-import transactionsSubheader from './transactions/components/subheader';
-import transactions from './transactions/components/transactions';
-import connectivityService from './common/services/connectivity-service';
-import terms from './meta/components/terms';
-import privacy from './meta/components/privacy';
-import about from './meta/components/about';
-import contact from './meta/components/contact';
-import store from './redux/store';
-import * as transactionsActions from './redux/transactions/action-creators';
-import * as historyActions from './redux/history/action-creators';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, browserHistory } from 'react-router'
 
-const router = sheetRouter((r) => {
-  return [
-    r('/', () => layout({
-      header: dashboardSubheader,
-      content: dashboard
-    })),
-    r('/transactions', () => layout({
-      header: transactionsSubheader,
-      content: transactions
-    })),
-    r('/categories', () => layout({
-      header: categoriesSubheader,
-      content: categories
-    })),
-    r('/analytics', () => layout({
-      header: analyticsSubheader,
-      content: analytics
-    })),
-    r('/profile', () => layout({
-      header: profileSubheader,
-      content: profile
-    })),
-    r('/terms', () => layout({
-      content: terms
-    })),
-    r('/privacy', () => layout({
-      content: privacy
-    })),
-    r('/about', () => layout({
-      content: about
-    })),
-    r('/contact', () => layout({
-      content: contact
-    }))
-  ];
+const App = React.createClass({
+  render() {
+    return <div>It works</div>;
+  }
 });
 
-const appContainer = document.querySelector('.app-container');
-
-appContainer.appendChild(router(document.location.href));
-
-href(href => store.dispatch(historyActions.navigate(href)));
-history(href => store.dispatch(historyActions.navigate(href)));
-
-store.subscribe(() => {
-  const state = store.getState();
-  const location = state.history.location;
-  const el = document.querySelector('#root');
-  yo.update(el, router(location));
-});
-
-// Check for offline activites
-connectivityService.registerListener();
-// Load our initial route into the state
-store.dispatch(historyActions.navigate(document.location.href));
-
-// Fetch our initial data
-store.dispatch(transactionsActions.retrieveTransactions());
+render((
+  <Router history={browserHistory}>
+    <Route path="*" component={App}/>
+  </Router>
+), document.querySelector('.app-container'));
