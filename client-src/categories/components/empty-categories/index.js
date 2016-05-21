23f335682cc9be58cedd1fca0785bfd8c690c445
Jmeas <jellyes2@gmail.com>
2016-05-20 18:55:28 -0700

    Reword empty categories page

--- client-src/categories/components/empty-categories/index.js
+++ client-src/categories/components/empty-categories/index.js
@@ -4,10 +4,11 @@ export default function EmptyCategories() {
   return (
     <div className="empty-resource-list">
       <div className="empty-resource-list-message">
-        Categories will appear here.
+        There are no categories.
       </div>
       <div className="empty-resource-list-explanation">
-        Related transactions can be grouped together using categories.
+        This page will list categories that you create. Related transactions
+        can be grouped together using categories.
       </div>
     </div>
   );
