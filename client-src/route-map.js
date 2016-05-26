c374b32e0776e05788014af71920e8251ddcbf00
Jmeas <jellyes2@gmail.com>
2016-05-25 21:18:50 -0700

    Remove unused file

--- client-src/route-map.js
+++
@@ -1,8 +0,0 @@
-// Maps paths to route names. This makes it easy to do
-// per-page logic without dealing with the pathnames themselves.
-export default {
-  '/': 'home',
-  '/transactions': 'transactions',
-  '/analytics': 'analytics',
-  '/profile': 'profile'
-};
