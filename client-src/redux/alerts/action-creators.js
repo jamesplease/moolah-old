a391103e5b0c628e431ce30ed2ac884248ecddce
Jmeas <jellyes2@gmail.com>
2016-07-26 22:59:32 -0700

    Add more tests for Redux alerts
    As part of #413

--- client-src/redux/alerts/action-creators.js
+++ client-src/redux/alerts/action-creators.js
@@ -3,15 +3,13 @@ import actionTypes from './action-types';
 
 // Queues an alert to be displayed
 export function pushAlert(alertDescription) {
-  return dispatch => {
-    const id = _.uniqueId('alert-');
-    dispatch({
-      type: actionTypes.PUSH_ALERT,
-      newAlert: {
-        ...alertDescription,
-        id
-      }
-    });
+  const id = _.uniqueId('alert-');
+  return {
+    type: actionTypes.PUSH_ALERT,
+    newAlert: {
+      ...alertDescription,
+      id
+    }
   };
 }
 
