9fb20434244bad3993fe5b4f38065a5c2869506b
Jmeas <jellyes2@gmail.com>
2016-07-27 18:35:39 -0700

    Add tests for EmptyCategories

--- client-src/categories/components/empty-categories/index.js
+++ client-src/categories/components/empty-categories/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function EmptyCategories() {
   return (
-    <div className="empty-resource-list">
-      <div className="empty-resource-list-message">
+    <div className="emptyResourceList">
+      <div className="emptyResourceList-message">
         There are no categories.
       </div>
-      <div className="empty-resource-list-explanation">
+      <div className="emptyResourceList-explanation">
         This page will list categories that you create. Related transactions
         can be grouped together using categories.
       </div>
---
+++ test/unit/client/categories/components/empty-categories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import EmptyCategories from '../../../../../client-src/categories/components/empty-categories';
+
+describe('EmptyCategories', function() {
+  describe('rendering', () => {
+    beforeEach(() => {
+      this.wrapper = shallow(<EmptyCategories/>);
+    });
+
+    it('should have the right class name', () => {
+      expect(this.wrapper.hasClass('emptyResourceList')).to.be.ok;
+    });
+
+    it('should have a message with the right text', () => {
+      const message = this.wrapper.find('.emptyResourceList-message');
+      expect(message.text()).to.equal('There are no categories.');
+    });
+
+    it('should have an explanation with the right text', () => {
+      const explanation = this.wrapper.find('.emptyResourceList-explanation');
+      const text = 'This page will list categories that you create. Related transactions can be grouped together using categories.';
+      expect(explanation.text()).to.equal(text);
+    });
+  });
+});
