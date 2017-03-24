e0b0c7f89f4399b0fc1afb72f5f0656892316384
James Smith <jellyes2@gmail.com>
2017-03-23 23:35:02 -0700

    Begin to add support for user accounts

--- migrations/iq8imv3h.transactions.sql
+++ migrations/iq8imv3h.transactions.sql
@@ -4,7 +4,8 @@ CREATE TABLE transaction (
   value NUMERIC(9, 2) NOT NULL,
   date DATE,
   created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  updated_at TIMESTAMPTZ
+  updated_at TIMESTAMPTZ,
+  user_account_id INTEGER REFERENCES user_account(id) NOT NULL
 );
 
 CREATE TRIGGER updated_at BEFORE UPDATE ON transaction
