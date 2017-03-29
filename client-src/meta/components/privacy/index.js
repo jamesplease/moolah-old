b71b15a3cd437b087d855baf9d2c19c4167805b8
James Smith <jellyes2@gmail.com>
2017-03-29 08:24:21 -0700

    Add basic privacy policy

--- client-src/meta/components/privacy/index.js
+++ client-src/meta/components/privacy/index.js
@@ -7,11 +7,34 @@ export default function Privacy() {
         <h1>
           Privacy Policy
         </h1>
+        <p className="paragraph">
+          Moolah is an alpha release application right now. That means it's not
+          ready to be used, aside from if you're interested in testing it out.
+        </p>
         <h2>
           Information We Collect
         </h2>
         <p className="paragraph">
-          Coming soon...
+          Moolah uses common technologies for all visitors, such as cookies,
+          to track the same basic information that all websites collect. This
+          occurs whether or not you have an account with us or not.
+        </p>
+        <p className="paragraph">
+          If you choose to make an account, we collect the minimum amount of
+          information necessary for you to use our service. You provide this
+          information at account creation time, and it typically includes a
+          name and an email address.
+        </p>
+        <p className="paragraph">
+          Presently, all information collected by Moolah is destroyed each time
+          the application is deployed. Releases typically occur numerous times
+          per week.
+        </p>
+        <h2>
+          How We Share the Information We Collect
+        </h2>
+        <p className="paragraph">
+          We do not share or sell any information we collect.
         </p>
       </div>
     </div>
