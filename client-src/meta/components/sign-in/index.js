f39f4837a88740517baa0a3760f96c70f7444648
James Smith <jellyes2@gmail.com>
2017-03-29 09:30:58 -0700

    Update appearance of sign in buttons

--- client-src/meta/components/sign-in/index.js
+++ client-src/meta/components/sign-in/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import loginServices from '../../../common/services/login-services';
 
-function generateServiceLink(serviceName) {
-  return serviceName.toLowerCase();
-}
-
-function getLoginServiceImgUrl(serviceName) {
-  return `/${serviceName.toLowerCase()}-logo@2x.png`;
-}
-
 export default function SignIn() {
   return (
     <div className="signIn container padded-container">
@@ -19,11 +11,9 @@ export default function SignIn() {
         {loginServices.map(service => (
           <li key={service.name}>
             <a
-              href={`/login/${generateServiceLink(service.name)}`}
-              className="signIn-serviceBtn">
-              <img
-                src={getLoginServiceImgUrl(service.name)}
-                className="signIn-serviceLogo"/>
+              href={`/login/${service.name.toLowerCase()}`}
+              className={`signIn-serviceBtn signIn_${service.name.toLowerCase()}Btn`}>
+              <i className={`signIn-serviceLogo zmdi zmdi-${service.name.toLowerCase()}`}/>
               {service.name}
             </a>
           </li>
