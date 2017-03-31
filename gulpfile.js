6dbf48f5cdc3914cd3271584686acba9fa1a25bc
Jmeas <jellyes2@gmail.com>
2017-03-30 22:31:40 -0700

    Convert to Webpack 2 and Webpack CLI
    Also fixes the browser test build

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
 const {cleanDist, cleanTmp} = require('./gulp/clean');
 const {lintJs, lintCss} = require('./gulp/lint');
-const {buildCss, buildJavaScript, build, work} = require('./gulp/build');
-const {test, coverage, watchTests, testBrowser} = require('./gulp/test');
+const {buildCss, build, work} = require('./gulp/build');
+const {test, coverage, watchTests} = require('./gulp/test');
 
 gulp.task('clean', cleanDist);
 gulp.task('clean-tmp', cleanTmp);
@@ -14,7 +14,6 @@ gulp.task('lint', ['lint-js', 'lint-css']);
 // Build a production version of the application
 gulp.task('build', build);
 gulp.task('build-css', buildCss);
-gulp.task('build-javascript', buildJavaScript);
 
 // Set up the application to be developed
 gulp.task('work', work);
@@ -25,7 +24,6 @@ gulp.task('test', ['lint'], test);
 gulp.task('watch-tests', ['lint'], watchTests);
 // Generate a coverage report
 gulp.task('coverage', ['lint'], coverage);
-// Set up a livereload environment for our spec runner `test/runner.html`
-gulp.task('test-browser', ['lint', 'clean-tmp'], testBrowser);
+gulp.task('pre-test-browser', ['lint', 'clean-tmp']);
 
 gulp.task('default', ['test']);
