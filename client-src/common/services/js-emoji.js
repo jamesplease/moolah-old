d01e862522242f8d2aeada225e10cdbe96e2bf22
Jmeas <jellyes2@gmail.com>
2017-04-02 21:10:35 -0700

    Revert "Temporarily disable js-emoji"
    This reverts commit 199ffd445abc36a304213ca447bae61ccf6b5647.

--- client-src/common/services/js-emoji.js
+++ client-src/common/services/js-emoji.js
@@ -1,28 +1,28 @@
-// import EmojiConverter from 'js-emoji';
+import EmojiConverter from 'js-emoji';
+
 //
-// //
-// // This file exports an instance of `js-emoji`. It is used
-// // to turn a string, like `:smile:`, into HTML markup that the app
-// // will display as an emoji.
-// //
-// // Usage:
-// // `emoji.replace_colons(':smile:')`
-// //
-// // This can then be set as the HTML of an element to render an emoji.
-// //
+// This file exports an instance of `js-emoji`. It is used
+// to turn a string, like `:smile:`, into HTML markup that the app
+// will display as an emoji.
 //
-// const emoji = new EmojiConverter();
+// Usage:
+// `emoji.replace_colons(':smile:')`
 //
-// emoji.use_sheet = true;
-// emoji.img_sets = {
-//   apple: {
-//     sheet: '/sheet_apple_64.png',
-//     mask: 1
-//   }
-// };
-// // emoji.text_mode = true;
-// emoji.img_set = 'apple';
+// This can then be set as the HTML of an element to render an emoji.
 //
-// emoji.init_env();
-//
-// export default emoji;
+
+const emoji = new EmojiConverter();
+
+emoji.use_sheet = true;
+emoji.img_sets = {
+  apple: {
+    sheet: '/sheet_apple_64.png',
+    mask: 1
+  }
+};
+// emoji.text_mode = true;
+emoji.img_set = 'apple';
+
+emoji.init_env();
+
+export default emoji;
