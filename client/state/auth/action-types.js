c888baa8512ff8c5cd53df2f1b530bcad2eb589d
Jmeas <jellyes2@gmail.com>
2017-04-23 18:53:43 -0700

    Add ability to unlink media accounts

--- client/state/auth/action-types.js
+++ client/state/auth/action-types.js
@@ -1,5 +1,10 @@
 import keyMirror from 'keymirror';
 
 export default keyMirror({
-  UNAUTHORIZED: null
+  UNAUTHORIZED: null,
+  UNLINK_ACCOUNT: null,
+  UNLINK_ACCOUNT_FAIL: null,
+  UNLINK_ACCOUNT_SUCCEED: null,
+  UNLINK_ACCOUNT_ABORT: null,
+  UNLINK_ACCOUNT_RESET: null,
 });
--- client/state/auth/reducer.js
+++ client/state/auth/reducer.js
@@ -1,5 +1,72 @@
+import _ from 'lodash';
+import actionTypes from './action-types';
 import initialState from './initial-state';
+import xhrStatuses from '../util/xhr-statuses';
 
-export default (state = initialState) => {
-  return state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.UNLINK_ACCOUNT: {
+      const serviceName = action.serviceName;
+      return {
+        userMeta: _.merge(state.user, {
+          logins: {
+            [`${serviceName}UnlinkXhrStatus`]: xhrStatuses.PENDING
+          }
+        })
+      };
+    }
+
+    case actionTypes.UNLINK_ACCOUNT_ABORT: {
+      const serviceName = action.serviceName;
+      return {
+        userMeta: _.merge(state.user, {
+          logins: {
+            [`${serviceName}UnlinkXhrStatus`]: xhrStatuses.ABORTED
+          }
+        })
+      };
+    }
+
+    case actionTypes.UNLINK_ACCOUNT_FAIL: {
+      const serviceName = action.serviceName;
+      return {
+        userMeta: _.merge(state.user, {
+          logins: {
+            [`${serviceName}UnlinkXhrStatus`]: xhrStatuses.FAILED
+          }
+        })
+      };
+    }
+
+    case actionTypes.UNLINK_ACCOUNT_RESET: {
+      const serviceName = action.serviceName;
+      return {
+        userMeta: _.merge(state.user, {
+          logins: {
+            [`${serviceName}UnlinkXhrStatus`]: xhrStatuses.NULL
+          }
+        })
+      };
+    }
+
+    case actionTypes.UNLINK_ACCOUNT_SUCCEED: {
+      const serviceName = action.serviceName;
+      return {
+        userMeta: _.merge(state.user, {
+          logins: {
+            [`${serviceName}UnlinkXhrStatus`]: xhrStatuses.SUCCEEDED
+          }
+        }),
+        user: _.merge(state.user, {
+          logins: {
+            [serviceName]: false
+          }
+        })
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
 };
